e6f7988e5f982058f49aa85b6900547e6490bce0
dcasiano <82372508+dcasiano@users.noreply.github.com>
2022-05-03 13:53:13 +0200

    popup de mejoras (falta ajustar tamaños)

--- DSI Hito 5/DSI Hito 5/PlayingPage.xaml.cs
+++ DSI Hito 5/DSI Hito 5/PlayingPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.ComponentModel;
 using Windows.System;
+using Windows.UI;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,6 +28,7 @@ namespace DSI_Hito_5
         public int turnNumber;
         public int moneyCount;
         public int moneyPerRound;
+        public int selectedUpgrade;
         public event PropertyChangedEventHandler PropertyChanged;
 
         enum Menu
@@ -44,6 +46,10 @@ namespace DSI_Hito_5
             turnNumber = 1;
             moneyCount = 10;
             moneyPerRound = 5;
+            selectedUpgrade = 0;
+            UpgradeNameText.Text = "";
+            UpgradeDescriptionText.Text = "";
+            PropertyChanged += SetUpgradeInfo;
             openMenu = Menu.None;
         }
 
@@ -62,8 +68,9 @@ namespace DSI_Hito_5
 
         private void UpgradeButton_Click(object sender, RoutedEventArgs e)
         {
-            openMenu = Menu.Upgrade;
-            Frame.Navigate(typeof(Upgrades));
+            //openMenu = Menu.Upgrade;
+            //Frame.Navigate(typeof(Upgrades));
+            UpgradesPopup.IsOpen = true;
         }
 
         private void VillagersButton_Click(object sender, RoutedEventArgs e)
@@ -78,6 +85,119 @@ namespace DSI_Hito_5
                 e.Handled = true;
                 UpgradeButton.Focus(FocusState.Programmatic);
             }
+        }
+
+        // Popup
+
+        private void SetUpgradeInfo(object sender, PropertyChangedEventArgs e)
+        {
+            UpgradeNameText.Text = Model.GetMejoraById(selectedUpgrade).Nombre;
+            UpgradeDescriptionText.Text = Model.GetMejoraById(selectedUpgrade).Descripcion;
+
+            if (Model.GetMejoraById(selectedUpgrade).Desbloqueada && !Model.GetMejoraById(selectedUpgrade).Comprada)
+            {
+                UpgradePriceText.Text = Model.GetMejoraById(selectedUpgrade).Precio.ToString();
+                BuyButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BuyButton.Visibility = Visibility.Collapsed;
+
+                if (Model.GetMejoraById(selectedUpgrade).Comprada)
+                {
+                    UpgradePriceText.Text = "Comprada!";
+                }
+                else
+                {
+                    UpgradePriceText.Text = "Bloqueada";
+                }
+            }
+        }
+
+
+        private void Upgrade0_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 0;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade1_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 1;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade2_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 2;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade3_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 3;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade4_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 4;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade5_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 5;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private void Upgrade6_Click(object sender, RoutedEventArgs e)
+        {
+            selectedUpgrade = 6;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(selectedUpgrade)));
+        }
+
+        private SymbolIcon getUpgradeIcon(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return Icon0;
+                case 1:
+                    return Icon1;
+                case 2:
+                    return Icon2;
+                case 3:
+                    return Icon3;
+                case 4:
+                    return Icon4;
+                case 5:
+                    return Icon5;
+                case 6:
+                    return Icon6;
+                default:
+                    return Icon0;
+            }
+        }
+
+
+        private void BuyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Model.GetMejoraById(selectedUpgrade).Comprada = true;
+
+            getUpgradeIcon(selectedUpgrade).Foreground = new SolidColorBrush(Colors.RoyalBlue);
+            BuyButton.Visibility = Visibility.Collapsed;
+            UpgradePriceText.Text = "Comprada!";
+
+            if (selectedUpgrade != 6)
+            {
+                Model.GetMejoraById(selectedUpgrade + 1).Desbloqueada = true;
+
+                if (!Model.GetMejoraById(selectedUpgrade + 1).Comprada)
+                    getUpgradeIcon(selectedUpgrade + 1).Foreground = new SolidColorBrush(Colors.White);
+            }
+
+            // Gastar dinero
         }
     }
 }
