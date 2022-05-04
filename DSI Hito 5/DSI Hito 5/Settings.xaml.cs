9d051670c34f84774833c99dc107ecbb64028832
dcasiano <82372508+dcasiano@users.noreply.github.com>
2022-05-04 20:59:22 +0200

    ajustes menu principal

--- DSI Hito 5/DSI Hito 5/Settings.xaml.cs
+++ DSI Hito 5/DSI Hito 5/Settings.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.ComponentModel;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,14 +21,16 @@ namespace DSI_Hito_5
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class Settings : Page
+    public sealed partial class Settings : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         double volumeSaved;
-        ColorSpectrum color;
+        enum graphicsAllowed { Low, Medium, High, Ultra};
+        graphicsAllowed graphics;
         public Settings()
         {
             this.InitializeComponent();
-            
+            graphics = graphicsAllowed.High;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -44,6 +47,26 @@ namespace DSI_Hito_5
             }
             else VolumeAdjust.Value = volumeSaved;
 
+        }
+
+        private void DecreaseGraphics_Click(object sender, RoutedEventArgs e)
+        {
+            if (graphics > graphicsAllowed.Low)
+            {
+                graphics--;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(graphics)));
+            }
+
+            
+        }
+
+        private void IncreaseGraphics_Click(object sender, RoutedEventArgs e)
+        {
+            if (graphics < graphicsAllowed.Ultra)
+            {
+                graphics++;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(graphics)));
+            }
         }
     }
 }
